23174e4e29152f43a652bb2a61e33c7e4b99c542
wdhzw <e0376960@u.nus.edu>
2024-07-09 23:27:31 +0800

    fix: modify file structure

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use('/api/model', modelRoutes);
 app.use('/api/upload', uploadRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
